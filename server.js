08917ee912ae8b65ccf1b29c7f5462b6db5ab147
jessicamcinchak <jessica.mcinchak@gmail.com>
2015-07-01 14:08:44 -0400

    create /public dir, add express to serve static files

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
-var http = require('http'),
-	fs = require('fs');
+var express = require('express');
 
-//Create a simple HTTP server
-var server = http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.write("<h1>Hello!</h1><p>You asked for <code>" + request.url + "</code></p>");
-	response.end();
-});
-server.listen(1337); //set a listening port
+var server = express();
+server.use(express.static(__dirname + '/public'));
 
-// Read a file
-fs.readFile('something.txt', 'utf8', function(error, text) {
-	if (error) {
-		throw error;
-	}
-	console.log('The file contains:', text); //logs last
+var port = 10001;
+server.listen(port, function() {
+	console.log('server listening on port ' + port);
 });
 
-//Write a file
-fs.writeFile('something.txt', 'Node was here', function(error) {
-	if (error) {
-		console.log('Failed to write file:', error);
-	} else {
-		console.log('File written.'); //logs second
-	}
-});
+// var http = require('http'),
+// 	fs = require('fs'),
+// 	url = require('url');
 
-console.log('Server running at http://localhost:1337/'); //logs first
+// //Create a simple HTTP server
+// var server = http.createServer(function(request, response) {
+// 	response.writeHead(200, {'Content-Type': 'text/html'});
+// 	response.write("<h1>Hello!</h1><p>You asked for <code>" + request.url + "</code></p>");
+// 	response.end();
+// 	return;
+// });
+// server.listen(1337); //set a listening port
+
+// // Read a file
+// fs.readFile('something.txt', 'utf8', function(error, text) {
+// 	if (error) {
+// 		throw error;
+// 	}
+// 	console.log('The file contains:', text); //logs last
+// });
+
+// //Write a file
+// fs.writeFile('something.txt', 'Node was here', function(error) {
+// 	if (error) {
+// 		console.log('Failed to write file:', error);
+// 	} else {
+// 		console.log('File written.'); //logs second
+// 	}
+// });
+
+// console.log('Server running at http://localhost:1337/'); //logs first
