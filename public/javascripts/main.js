779f214c2563eecc912466ab1c5f232f988aac47
Jessica McInchak <jessica.mcinchak@gmail.com>
2015-07-15 16:17:17 -0400

    clean comments

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,11 +5,13 @@ var config = {
 };
 
 $(function() {
-	// Activate editable on click by activating closest editable parent
+	/** Activate editable on click by activating closest editable parent */
 	$('.editable').on('click', $.fn.halloActivateClosestEditableParent.bind($(this)));
 
-	// Use hallo events to track modified status of editable
-	// Link multiple actions onto same class selector
+	/**
+	 * Use hallo events to track modified status of editable
+	 * Link multiple actions onto same class selector
+	 */
 	var $modified = $('#modified');
 	$('.editable').bind('hallomodified', function(event, data) {
 		$modified.html("Editable has been modified"); //detects adding and deleting characters
@@ -19,41 +21,28 @@ $(function() {
 		$modified.html("Selection removed"); //detects un-highlighting
 	});
 
-	// Paste csv data into editable, save out as JSON
+	/** Paste csv data into editable, save out as JSON */
 	$('.editable--spreadsheet').bind('hallodeactivated', function() {
 		var $this = $(this),
 			html = $this.html();
 			// Add $().tableToJSON() here to convert
 	});
 
-	// Click link to add new editable
+	/** Click link to add new editable */
 	$('body').on('click', '.add-editable', function() {
 		var $editable = $('<div class="editable"><p></p></div>');
 		$editable.insertBefore($(this));
-		// Apply hallo event listeners to newly added elements
+		// Apply hallo event listeners to newly added elements individually. Once React, we'll eventually do this in a batch
 		$editable.on('click', $.fn.halloActivateClosestEditableParent.bind($(this)));
 	});
 
-	// Double click on wrapper to deactivate all editables
+	/** Double click on wrapper to deactivate all editables */
 	$('.wrapper').on('dblclick', $.fn.halloDeactivate.bind($('.editable')));
 
-	// Click link to save contents of all editables within the wrapper to a file
+	/** Click link to save contents of all editables within the wrapper to a file */
 	var fileName = 'test.html';
 	$('.save-editables').click(function(){
     	downloadInnerHtml(fileName, 'main','text/html');
 	});
 
-	// // Future use - introduce react classes for hallo editables
-	// React.createClass({
-	// 	componentDidMount: function() {
-	// 		$this = React.findDOMNode(this);
-	// 		this.clickCallback = $.fn.halloActivateClosestEditableParent.bind($(this));
-	// 		$this.on('click', this.clickCallback);
-	// 	},
-	// 	componentWillUnmount: function() {
-	// 		$this.off('click');
-	// 	},
-	// 	render: function() {} //this should return div
-	// 	// how to deal with state? 
-	// });
 });
